7c709562d6a8cd22899ce212ace60087a3a7bdf8
Doug Wight <dcfwight@gmail.com>
2022-02-02 10:17:33 +0000

    Updated comments to explain

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -16,27 +16,7 @@
 #   * A GET request whose path contains a short name.  The server looks up
 #     that short name in its dictionary and redirects to the corresponding
 #     long URI.
-#
-# Your job in this exercise is to finish the server code.
-#
-# Here are the steps you need to complete:
-#
-# 1. Write the CheckURI function, which takes a URI and returns True if a
-#    request to that URI returns a 200 OK, and False otherwise.
-#
-# 2. Write the code inside do_GET that sends a 303 redirect to a known name.
-#
-# 3. Write the code inside do_POST that sends a 303 redirect to the form
-#    after saving a newly submitted URI.
-#
-# 4. Write the code inside do_POST that sends a 404 error if a URI is not
-#    successfully checked (i.e. if CheckURI returns false).
-#
-# In each step, you'll need to delete a line of code that raises the
-# NotImplementedError exception.  These are there as placeholders in the
-# starter code.
-#
-# After writing each step, restart the server and run test.py to test it.
+
 
 import os
 import http.server
@@ -45,23 +25,25 @@ from urllib.parse import unquote, parse_qs
 import threading
 from socketserver import ThreadingMixIn
 
+
 class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
-    "This is an HTTPServer that supports thread-based concurrency."
+	"""This is an HTTPServer that supports thread-based concurrency."""
+
 
 memory = {}
 
 form = '''<!DOCTYPE html>
 <title>Bookmark Server</title>
 <form method="POST">
-    <label>Long URI:
-        <input name="longuri">
-    </label>
-    <br>
-    <label>Short name:
-        <input name="shortname">
-    </label>
-    <br>
-    <button type="submit">Save it!</button>
+	<label>Long URI:
+		<input name="longuri">
+	</label>
+	<br>
+	<label>Short name:
+		<input name="shortname">
+	</label>
+	<br>
+	<button type="submit">Save it!</button>
 </form>
 <p>URIs I know about:
 <pre>
@@ -70,84 +52,87 @@ form = '''<!DOCTYPE html>
 '''
 
 
-def CheckURI(uri, timeout=5):
-    '''Check whether this URI is reachable, i.e. does it return a 200 OK?
+def checkURI(uri, timeout=5):
+	"""Check whether this URI is reachable, i.e. does it return a 200 OK?
 
     This function returns True if a GET request to uri returns a 200 OK, and
     False if that GET request returns any other response, or doesn't return
     (i.e. times out).
-    '''
-    # 1. Write this function.
-    #print('checking uri: ' +str(uri))
-    try:
-        r = requests.get(uri, timeout=timeout)
-        print ('r.status_code: ' +str(r.status_code))
-        return r.status_code == 200
-            
-    except requests.RequestException:
-        print ("could not check URI")
-        return False
-    
+    """
+
+	try:
+		r = requests.get(uri, timeout=timeout)
+		print('r.status_code: ' + str(r.status_code))
+		return r.status_code == 200
+
+	except requests.RequestException:
+		print("could not check URI")
+		return False
+
+
 class Shortener(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        # A GET request will either be for / (the root path) or for /some-name.
-        # Strip off the / and we have either empty string or a name.
-        name = unquote(self.path[1:])
+	def do_GET(self):
+		# A GET request will either be for / (the root path) or for /some-name.
 
-        if name:
-            if name in memory:
-                # 2. Send a 303 redirect to the long URI in memory[name].
-                self.send_response(303)
-                self.send_header('Location',memory[name]) # this is where to redirect the browser to.
-                self.end_headers()
-                
-            else:
-                # We don't know that name! Send a 404 error.
-                self.send_response(404)
-                self.send_header('Content-type', 'text/plain; charset=utf-8')
-                self.end_headers()
-                self.wfile.write("I don't know '{}'.".format(name).encode())
-        else:
-            # Root path. Send the form.
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            # List the known associations in the form.
-            known = "\n".join("{} : {}".format(key, memory[key])
-                              for key in sorted(memory.keys()))
-            self.wfile.write(form.format(known).encode())
+		name = unquote(self.path[1:])
 
-    def do_POST(self):
-        # Decode the form data.
-        length = int(self.headers.get('Content-length', 0))
-        body = self.rfile.read(length).decode()
-        params = parse_qs(body)
-        longuri = params["longuri"][0]
-        shortname = params["shortname"][0]
+		if name:
+			if name in memory:
+				# 2. Send a 303 redirect to the long URI in memory[name].
+				self.send_response(303)
+				self.send_header('Location', memory[name])  # this is where to redirect the browser to.
+				self.end_headers()
 
-        if CheckURI(longuri):
-            # This URI is good!  Remember it under the specified name.
-            memory[shortname] = longuri
+			else:
+				# We don't know that name! Send a 404 error.
+				self.send_response(404)
+				self.send_header('Content-type', 'text/plain; charset=utf-8')
+				self.end_headers()
+				self.wfile.write("I don't know '{}'.".format(name).encode())
+		else:
+			# Root path. Send the form.
+			self.send_response(200)
+			self.send_header('Content-type', 'text/html')
+			self.end_headers()
+			# List the known associations in the form.
+			known = "\n".join("{} : {}".format(key, memory[key])
+							  for key in sorted(memory.keys()))
+			self.wfile.write(form.format(known).encode())
 
-            # 3. Serve a redirect to the root page (the form).
-            self.send_response(303)
-            self.send_header('Location','/') # this is where to redirect the browser to.
-            self.end_headers()
-            
-        else:
-            # Didn't successfully fetch the long URI.
+	def do_POST(self):
+		# Decode the form data.
+		length = int(self.headers.get('Content-length', 0))
+		body = self.rfile.read(length).decode()
+		params = parse_qs(body)
+		longuri = params["longuri"][0]
+		shortname = params["shortname"][0]
 
-            # 4. Send a 404 error with a useful message.
-            self.send_response(404)
-            self.send_header('Content-type', 'text/plain; charset=utf-8')
-            self.end_headers()
-            self.wfile.write("We could not find that URI {} - did you check it works first?".format(longuri).encode())
+		if checkURI(longuri):
+			# This URI is good!  Remember it under the specified name.
+			memory[shortname] = longuri
+
+			# 3. Serve a redirect to the root page (the form).
+			self.send_response(303)
+			self.send_header('Location', '/')  # this is where to redirect the browser to.
+			self.end_headers()
+
+		else:
+			# Didn't successfully fetch the long URI.
+
+			# 4. Send a 404 error with a useful message.
+			self.send_response(404)
+			self.send_header('Content-type', 'text/plain; charset=utf-8')
+			self.end_headers()
+			self.wfile.write("We could not find that URI {} - did you check it works first?".format(longuri).encode())
+
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000)) # So the program checks os.environ.get('PORT') - if on Heroku, will be given a port
-    # if it's not on Heroku (or other server), then it won't find a port - so give it 8000, which is the local port. So this will
-    # check to see if it runs on server - if not, default to local.
-    server_address = ('', port)
-    httpd = ThreadHTTPServer(server_address, Shortener)
-    print ('HTTPServer listening on port: ' +str(port))
-    httpd.serve_forever()
+	port = int(os.environ.get('PORT', 8000))
+	# So the program checks os.environ.get('PORT') - if on Heroku, will be given a port
+	# if it's not on Heroku (or other server), then it won't find a port - so give it 8000,
+	# which is the local port. So this will
+	# check to see if it runs on server - if not, default to local.
+	server_address = ('', port)
+	httpd = ThreadHTTPServer(server_address, Shortener)
+	print('HTTPServer listening on port: ' + str(port))
+	httpd.serve_forever()
